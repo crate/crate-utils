6348bd2d33899d4279d5a79c4b1f3f133cf85ba0
admin <admin@de1.ls.af>
2014-02-21 17:01:38 +0100

    working installer for rpm systems

--- easy2install/crate-install.sh
+++ easy2install/crate-install.sh
@@ -52,7 +52,7 @@ It looks like you hit an issue when trying to install Crate Data.
 
 Troubleshooting and basic usage information for Crate Data are available at:
 
-    https://crate.io/documentation/
+    https://crate.io/docs/
 
 If you're still having problems, please send an email to support@crate.io
 with the contents of crate-install.log and we'll do our very best to help you
@@ -90,24 +90,30 @@ fi
 
 PY_VERSION=$(python -c 'import sys; print "%d.%d" % (sys.version_info[0], sys.version_info[1])')
 
-if [ $PY_VERSION = "2.4" -o $PY_VERSION = "2.5" ]; then
-    DDBASE=true
-else
-    DDBASE=false
-fi
 
 # Install the necessary package sources
 if [ $OS = "RedHat" ]; then
-    echo -e "\033[34m\n* Installing YUM sources for Datadog\n\033[0m"
-    sudo sh -c "echo -e '[datadog]\nname = Datadog, Inc.\nbaseurl = http://yum.datadoghq.com/rpm/\nenabled=1\ngpgcheck=0\npriority=1' > /etc/yum.repos.d/datadog.repo"
 
-    printf "\033[34m* Installing the Datadog Agent package\n\033[0m\n"
-
-    if $DDBASE; then
-        sudo yum -y install datadog-agent-base
+    if [ $(rpm -q crate-release) ]; then
+        echo -e "\033[34m* The crate repository is already installed\033[0m"
     else
-        sudo yum -y install datadog-agent
+        echo -e "\033[34m\n* Installing YUM sources for Crate\n\033[0m"
+        sudo sh -c "sudo rpm --import https://cdn.crate.io/downloads/yum/RPM-GPG-KEY-crate"
+        sudo sh -c "sudo rpm -Uvh https://cdn.crate.io/downloads/yum/6/x86_64/crate-release-6.5-1.noarch.rpm"
     fi
+
+    if [ $(rpm -q crate) ]; then
+        echo -e "\033[34m* The Crate package is already installed\033[0m"
+    else
+        printf "\033[34m\n* Installing the Crate package\n\033[0m\n"
+        sudo sh -c "sudo yum -y install crate"
+    fi
+
+    printf "\033[34m* Starting the Service...\n\033[0m\n"
+    sudo mkdir -p /opt/crate/data/crate
+    sudo chown crate:crate /opt/crate/data/crate
+    sudo service crate start
+
 elif [ $OS = "Debian" -o $OS = "Ubuntu" ]; then
     printf "\033[34m\n* Installing APT package sources for Datadog\n\033[0m\n"
     sudo sh -c "echo 'deb http://apt.datadoghq.com/ unstable main' > /etc/apt/sources.list.d/datadog.list"
@@ -128,75 +134,14 @@ Please follow the instructions on the Agent setup page:
     exit;
 fi
 
-printf "\033[34m\n* Adding your API key to the Agent configuration: /etc/dd-agent/datadog.conf\n\033[0m\n"
 
-if $DDBASE; then
-    sudo sh -c "sed 's/api_key:.*/api_key: $apikey/' /etc/dd-agent/datadog.conf.example | sed 's/# dogstatsd_target :.*/dogstatsd_target: https:\/\/app.datadoghq.com/' > /etc/dd-agent/datadog.conf"
-else
-    sudo sh -c "sed 's/api_key:.*/api_key: $apikey/' /etc/dd-agent/datadog.conf.example > /etc/dd-agent/datadog.conf"
-fi
-
-printf "\033[34m* Starting the Agent...\n\033[0m\n"
-sudo /etc/init.d/datadog-agent restart
-
-# Datadog "base" installs don't have a forwarder, so we can't use the same
-# check for the initial payload being sent.
-if $DDBASE; then
 printf "\033[32m
-Your Agent has started up for the first time and is submitting metrics to
-Datadog. You should see your Agent show up in Datadog shortly at:
+Your Crate Service has started up for the first time.
 
-    https://app.datadoghq.com/infrastructure\033[0m
-
-If you ever want to stop the Agent, run:
-
-    sudo /etc/init.d/datadog-agent stop
-
-And to run it again run:
-
-    sudo /etc/init.d/datadog-agent start
+To checkout the admin UI open http://localhost:4200/admin in your browser
+\033[0m
 "
-exit;
-fi
 
-# Wait for metrics to be submitted by the forwarder
-printf "\033[32m
-Your Agent has started up for the first time. We're currently verifying that
-data is being submitted. You should see your Agent show up in Datadog shortly
-at:
+# exit successfully for now...
+exit 0
 
-    https://app.datadoghq.com/infrastructure\033[0m
-
-Waiting for metrics..."
-
-c=0
-while [ "$c" -lt "30" ]; do
-    sleep 1
-    echo -n "."
-    c=$(($c+1))
-done
-
-$dl_cmd http://127.0.0.1:17123/status?threshold=0 > /dev/null 2>&1
-success=$?
-while [ "$success" -gt "0" ]; do
-    sleep 1
-    echo -n "."
-    $dl_cmd http://127.0.0.1:17123/status?threshold=0 > /dev/null 2>&1
-    success=$?
-done
-
-# Metrics are submitted, echo some instructions and exit
-printf "\033[32m
-
-Your Agent is running and functioning properly. It will continue to run in the
-background and submit metrics to Datadog.
-
-If you ever want to stop the Agent, run:
-
-    sudo /etc/init.d/datadog-agent stop
-
-And to run it again run:
-
-    sudo /etc/init.d/datadog-agent start
-
-\033[0m"
