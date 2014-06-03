bc076e21c13a2750bb607bb3ee5d4cdb1dc51f79
Christian Haudum <christian.haudum@gmail.com>
2014-06-03 21:33:47 +0200

    increased crate version

--- easy/try.sh
+++ easy/try.sh
@@ -32,7 +32,7 @@ INV="\033[7m"
 BRN="\033[33m"
 RED="\033[31m"
 END="\033[0m\033[27m"
-VERSION="0.38.2"
+VERSION="0.38.3"
 
 function wait_for_user() {
     read -p "Press RETURN to continue or any other key to abort" -n1 -s x
