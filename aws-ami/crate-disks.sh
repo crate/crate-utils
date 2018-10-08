ec5f2b776e4d825b8a595534e302aa8cd8259554
Markus Holtermann <markus@crate.io>
2018-10-08 16:22:08 +0200

    Fixed shellcheck remarks in aws-ami/crate-disks.sh script

--- aws-ami/crate-disks.sh
+++ aws-ami/crate-disks.sh
@@ -10,26 +10,25 @@ DEVICES=$(curl -s "$API/block-device-mapping/" | grep -v 'ami\|root')
 for NAME in $DEVICES
 do
   DEV=$(curl -s "$API/block-device-mapping/$NAME")
-  IS_FORMATTED=$(file -sL /dev/$DEV | grep "filesystem")
+  IS_FORMATTED=$(file -sL "/dev/$DEV" | grep "filesystem")
 
-  if [ ! -z "$IS_FORMATTED" -a "$IS_FORMATTED" != " " ]; then
+  if [ ! -z "$IS_FORMATTED" ] && [ "$IS_FORMATTED" != " " ]; then
     echo "Device is formatted and ready to mount!"
   else
     echo "Device is not formatted"
     # try to format the device
-    mkfs.ext4 -F /dev/$DEV
+    mkfs.ext4 -F "/dev/$DEV"
   fi
 
   # mount the device
-  mkdir -p /mnt/$DEV
-  mount /dev/$DEV /mnt/$DEV
-  if [ $? -eq 0 ]; then
+  mkdir -p "/mnt/$DEV"
+  if mount "/dev/$DEV" "/mnt/$DEV" ; then
     echo "/dev/$DEV   /mnt/$DEV   auto  defaults,nofail,comment=cratedisks  0 2" >> /etc/fstab
     echo "mount successful!"
   else
     echo "mount failed!"
   fi
 
-  mkdir -p /mnt/$DEV/crate
-  chown -R crate:crate /mnt/$DEV/crate
+  mkdir -p "/mnt/$DEV/crate"
+  chown -R crate:crate "/mnt/$DEV/crate"
 done
